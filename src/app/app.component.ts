47bc454ff845b6cc64adf9cfba4257feb0bb7033
Obada Sawalmeh <obada-sawalmeh@obada-sawalmeh-linux.scoiumtech.local>
2024-11-14 17:51:22 +0300

    finished the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,73 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { employee } from './model/employee';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  empForm: FormGroup = new FormGroup({});
+  empObj: employee = new employee();
+  empList: employee[] = [];
+  show:boolean=false
+  constructor() {
+    this.createForm();
+    const oldData = localStorage.getItem('empData');
+    if (oldData) {
+      this.empList = JSON.parse(oldData);
+    }
+  }
+  createForm() {
+    this.empForm = new FormGroup({
+      empId: new FormControl(this.empObj.empId),
+      empName: new FormControl(this.empObj.empName),
+      empEmail: new FormControl(this.empObj.empEmail),
+      empNumber: new FormControl(this.empObj.empNumber),
+      empState: new FormControl(this.empObj.empState),
+      empAddress: new FormControl(this.empObj.empAddress),
+      empPassword: new FormControl(this.empObj.empPassword),
+    });
+  }
+  onSave() {
+    const oldData = localStorage.getItem('empData');
+    if (oldData) {
+      this.empForm.controls['empId'].setValue(this.empList.length + 1);
+      this.empList.unshift(this.empForm.value);
+    } else {
+      this.empList.unshift(this.empForm.value);
+    }
+    localStorage.setItem('empData', JSON.stringify(this.empList));
+    this.createForm();
+  }
+  delete(id: number) {
+    this.empList.splice(id, 1);
+    localStorage.setItem('empData', JSON.stringify(this.empList));
+  }
+  edit(item: employee) {
+    this.showEdit()
+    this.empObj = item;
+    this.createForm();
+    this.showEdit()
+  }
+  showEdit(){
+    this.show = true
+    return this.show
+  }
+  onUpdate(id: number) {
+    this.show = false
+    const record = this.empList.find((x) => x.empId === id);
+    if (record) {
+      record.empName = this.empForm.controls['empName'].value;
+      record.empEmail = this.empForm.controls['empEmail'].value;
+      record.empNumber = this.empForm.controls['empNumber'].value;
+      record.empState = this.empForm.controls['empState'].value;
+      record.empAddress = this.empForm.controls['empAddress'].value;
+      record.empPassword = this.empForm.controls['empPassword'].value;
+    }
+    localStorage.setItem('empData', JSON.stringify(this.empList));
+    this.empForm.reset();
+  }
 }
---
+++ src/app/model/employee.ts
@@ -0,0 +1,18 @@
+export class employee {
+  empId: number;
+  empName: string;
+  empEmail: string;
+  empNumber: string;
+  empState: string;
+  empAddress: string;
+  empPassword:string;
+  constructor() {
+    this.empId = 1;
+    this.empName = '';
+    this.empEmail = '';
+    this.empNumber = '';
+    this.empState = '';
+    this.empAddress = '';
+    this.empPassword='';
+  }
+}
